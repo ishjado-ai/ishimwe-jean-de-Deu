6690258680f61c05f5cd00b2052149b001f1d86e
ishjado-ai <ishjado@gmail.com>
2025-11-17 12:59:08 +0200

    JS assesment

---
+++ index.js
@@ -0,0 +1,14 @@
+let text = " ISH JADO(SWEGER BOY)HEER WE GO ";
+ console.log("Length:", text.length);
+
+  console.log("string slice:",text.slice(1,6));
+
+  console.log("sub str",text.substr(1,5));
+  
+console.log( text.replace("JADO", "CHEAF"));
+
+console.log("split:", text.split(" "));
+
+console.log("Uppercase:", text.toUpperCase());
+
+console.log("Lowercase:", text.toLowerCase());
